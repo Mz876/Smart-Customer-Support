7f56f541ad14d42548d8ee74920324d65ea4d52a
Mustafa Zabet <mustafazabet55@gmail.com>
2025-10-25 02:26:46 +0300

    add connection

---
+++ backend/config/db.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = 'localhost';
+$db   = 'smartcustsupport';
+$user = 'root';
+$password = '';
+$port = 3307;
+
+$conn = mysqli_connect($host, $user, $password, $db,$port);
+
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+echo "Connected successfully";
+
+
+
+?>
